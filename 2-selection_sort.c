c0686b048ec60fab2b88c87a894b51c9b99c7269
Daniel Berthran <berthlearn@gmail.com>
2023-04-20 12:00:50 +0100

    Create mandatory files - no code yet

---
+++ 2-selection_sort.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "sort.h"
+
+/**
+ * selection_sort - 
+ * @array:
+ * @size:
+ *
+ * Return: nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	pass;
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "sort.h"
+
+/**
+ * quick_sort - 
+ * @array:
+ * @size:
+ *
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	pass;
+}
