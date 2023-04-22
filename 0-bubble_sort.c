cc81d5542c5d209b3537980505d96c37fe7f4cd5
Daniel Berthran <berthlearn@gmail.com>
2023-04-22 16:22:38 +0100

    solve 0-bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,15 +3,48 @@
 #include <string.h>
 #include "sort.h"
 
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
 /**
- * bubble_sort - 
- * @array:
- * @size:
+ * bubble_sort - a sorting algorithm based on Bubble sort
+ * @array: array to be sorted
+ * @size: size of the array
  *
  * Return: nothing
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	pass;
+	size_t i, iter, new_size;
+	int temp = 0;
+	int unsorted = TRUE;
+
+	iter = size - 1;
+
+	/* Traveersing the array */
+	while (unsorted)
+	{
+		unsorted = FALSE;
+		/* Comparing elements */
+		for (i = 0; i < iter; i++)
+		{
+			/* Swapping elements */
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				unsorted = TRUE;
+				new_size = i;
+				print_array(array, size);
+			}
+		}
+		iter = new_size;
+	}
 }
